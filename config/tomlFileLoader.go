ce0cba07ab61dfef1963d889821e8e13be395640
Augusto Dwenger J <dwenger@posteo.de>
2020-10-29 09:45:30 +0100

    Simplify the config reading

--- config/tomlFileLoader.go
+++ config/tomlFileLoader.go
@@ -1,25 +1,34 @@
 package config
 
 import (
+	"io"
 	"io/ioutil"
-	"os"
+
+	"github.com/pelletier/go-toml"
 )
 
-// TomlFileLoader handels a toml configuration file
-type TomlFileLoader struct {
-	file *os.File
+// TomlLoader handels a toml configuration file
+type TomlLoader struct {
+	file io.Reader
 }
 
 // Load implementation of the Loader interface to load the config from the file
-func (tfl TomlFileLoader) Load() (Data, error) {
-	data, err := ioutil.ReadAll(tfl.file)
+func (tl TomlLoader) Load() (Data, error) {
+	tomlData, err := ioutil.ReadAll(tl.file)
 	if err != nil {
 		return Data{}, err
 	}
-	return tomlLoader{toml: &data}.Load()
+
+	data := Data{}
+	err = toml.Unmarshal(tomlData, &data)
+	if err != nil {
+		return Data{}, err
+	}
+
+	return data, nil
 }
 
 // NewTomlFileLoader constructor for the TomlFileLoader struct
-func NewTomlFileLoader(file *os.File) TomlFileLoader {
-	return TomlFileLoader{file: file}
+func NewTomlFileLoader(file io.Reader) TomlLoader {
+	return TomlLoader{file: file}
 }
--- config/tomlFileLoader_test.go
+++ config/tomlFileLoader_test.go
@@ -1,22 +1,41 @@
 package config
 
 import (
-	"os"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTomlFileLoaderLoad(t *testing.T) {
-	file, err := os.Open("./test_files/config.toml")
-	if err != nil {
-		t.Fatal("test file not found")
+type mockFile struct {
+	content   []byte
+	readIndex int
+}
+
+func (f *mockFile) Read(b []byte) (n int, err error) {
+	if f.readIndex >= len(f.content) {
+		err = io.EOF
+		return
 	}
+	n = copy(b, f.content[f.readIndex:])
+	f.readIndex += n
+	return
+}
+
+func TestTomlFileLoaderLoad(t *testing.T) {
+	file := &mockFile{content: []byte(`[stats]
+	endpoint = "https://stats.example.com"
+	hostname = "test"
+	token = "xxx"
+	
+	[gotify]
+	endpoint = "https://push.example.com"
+	token = "xxx"`)}
 
 	statsConfig := Stats{Endpoint: "https://stats.example.com", Hostname: "test", Token: "xxx"}
 	gotifyConfig := Gotify{Endpoint: "https://push.example.com", Token: "xxx"}
 	want := Data{Stats: statsConfig, Gotify: gotifyConfig}
-	got, err := TomlFileLoader{file: file}.Load()
+	got, err := TomlLoader{file: file}.Load()
 
 	assert.Nil(t, err)
 	assert.Equal(t, want, got)
