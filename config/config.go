9d107104cc7b9f7758496a8104fce90feed7cb38
Augusto Dwenger <hamburghammer@gmail.com>
2020-10-29 11:06:48 +0100

    Add validateion for the Stats and Gotify config

--- config/config.go
+++ config/config.go
@@ -1,5 +1,7 @@
 package config
 
+import "errors"
+
 // Loader interface to load a config
 type Loader interface {
 	Load() (Data, error)
@@ -18,8 +20,37 @@ type Stats struct {
 	Token    string
 }
 
+// Validate checks each field of Stats
+func (s *Stats) Validate() []error {
+	errs := make([]error, 0)
+	if s.Endpoint == "" {
+		errs = append(errs, errors.New("Missing Endpoint inside the Stats config"))
+	}
+	if s.Hostname == "" {
+		errs = append(errs, errors.New("Missing Hostname inside the Stats config"))
+	}
+	if s.Token == "" {
+		errs = append(errs, errors.New("Missing Token inside the Stats config"))
+	}
+
+	return errs
+}
+
 // Gotify configuration for the gotify client
 type Gotify struct {
 	Endpoint string
 	Token    string
+}
+
+// Validate checks each field of Gotify
+func (g *Gotify) Validate() []error {
+	errs := make([]error, 0)
+	if g.Endpoint == "" {
+		errs = append(errs, errors.New("Missing Endpoint inside the Gotify config"))
+	}
+	if g.Token == "" {
+		errs = append(errs, errors.New("Missing Token inside the Gotify config"))
+	}
+
+	return errs
 }
