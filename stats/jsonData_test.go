20ede605fb89033a3dc04370539aa355f4da62c2
Augusto Dwenger J <dwenger@posteo.de>
2020-11-17 17:23:54 +0100

    Replace all occurrences from assert with require
    The package testify offers assert and require for testing.
    The difference between assert and require are is that on require
    the test will immediately fail.

--- stats/jsonData_test.go
+++ stats/jsonData_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func getJSONData() jsonData {
@@ -18,8 +18,8 @@ func TestTransformation(t *testing.T) {
 		want := getJSONData().Date
 		got, err := getJSONData().transformToData()
 
-		assert.Nil(t, err)
-		assert.Equal(t, want, got.Date.Format(time.RFC3339))
+		require.Nil(t, err)
+		require.Equal(t, want, got.Date.Format(time.RFC3339))
 	})
 
 	t.Run("should wrap transforming iso date error", func(t *testing.T) {
@@ -29,39 +29,39 @@ func TestTransformation(t *testing.T) {
 		want := "transforming jsonData to Data: parsing the json Date string produced an error: parsing time \"2020-08-19T15:18:06+02:00xxx\": extra text: \"xxx\""
 		_, got := wrongDateJSONData.transformToData()
 
-		assert.Equal(t, want, got.Error())
+		require.Equal(t, want, got.Error())
 	})
 
 	t.Run("should transform mem string into Memory struct", func(t *testing.T) {
 		want := Memory{Used: 100, Total: 1000}
 		got, err := getJSONData().transformToData()
 
-		assert.Nil(t, err)
-		assert.Equal(t, want, got.Mem)
+		require.Nil(t, err)
+		require.Equal(t, want, got.Mem)
 	})
 
 	t.Run("should transform disk string into Memory struct", func(t *testing.T) {
 		want := Memory{Used: 1000, Total: 10000}
 		got, err := getJSONData().transformToData()
 
-		assert.Nil(t, err)
-		assert.Equal(t, want, got.Disk)
+		require.Nil(t, err)
+		require.Equal(t, want, got.Disk)
 	})
 
 	t.Run("should add CPU", func(t *testing.T) {
 		want := getJSONData().CPU
 		got, err := getJSONData().transformToData()
 
-		assert.Nil(t, err)
-		assert.Equal(t, want, got.CPU)
+		require.Nil(t, err)
+		require.Equal(t, want, got.CPU)
 	})
 
 	t.Run("should add Processes", func(t *testing.T) {
 		want := getJSONData().Processes
 		got, err := getJSONData().transformToData()
 
-		assert.Nil(t, err)
-		assert.Equal(t, want, got.Processes)
+		require.Nil(t, err)
+		require.Equal(t, want, got.Processes)
 	})
 }
 
@@ -75,7 +75,7 @@ func TestParseISODateString(t *testing.T) {
 			t.Errorf("An unexpected error happened: %s", err.Error())
 		}
 
-		assert.Equal(t, want, got.Format(time.RFC3339))
+		require.Equal(t, want, got.Format(time.RFC3339))
 	})
 
 	t.Run("should return error on wrong string", func(t *testing.T) {
@@ -84,8 +84,8 @@ func TestParseISODateString(t *testing.T) {
 		want := "parsing the json Date string produced an error: parsing time \"2020-08-19T15:18:06+02:00xxx\": extra text: \"xxx\""
 		_, err := jsonData{Date: currentTime}.parseDateToTime()
 
-		assert.NotNil(t, err)
-		assert.Equal(t, want, err.Error())
+		require.NotNil(t, err)
+		require.Equal(t, want, err.Error())
 	})
 }
 
@@ -97,22 +97,22 @@ func TestParseMemoryString(t *testing.T) {
 			t.Errorf("An unexpected error happened: %s", err.Error())
 		}
 
-		assert.Equal(t, want, got)
+		require.Equal(t, want, got)
 	})
 
 	t.Run("should return error if not seperated by a slash", func(t *testing.T) {
 		want := "the string is missing the separating slash like 100/1000: 100 1000"
 		_, err := jsonData{}.parseMemoryString("100 1000")
 
-		assert.NotNil(t, err)
-		assert.Equal(t, want, err.Error())
+		require.NotNil(t, err)
+		require.Equal(t, want, err.Error())
 	})
 
 	t.Run("should return error if not seperated by a slash", func(t *testing.T) {
 		want := "parsing parts of a the string for the Memory struct: strconv.ParseInt: parsing \"foo\": invalid syntax"
 		_, err := jsonData{}.parseMemoryString("foo/1000")
 
-		assert.NotNil(t, err)
-		assert.Equal(t, want, err.Error())
+		require.NotNil(t, err)
+		require.Equal(t, want, err.Error())
 	})
 }
